d2bbb950ba04a70d1ef5f6e0b0319450048106bd
fa-kohansal <farnaz_kohansal@yahoo.com>
2025-01-01 18:50:38 -0800

    create dropdown button for location list

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import "../public/css/main.css";
 import"../public/css/style.css"
 import"../node_modules/bootstrap-icons/font/bootstrap-icons.min.css"
-let locChoose=("click",function(){
-  
-})
+export function locChoose(){
+    let locationList=document.getElementById("locationList");
+    if(locationList.classList.contains("hidden")){
+        locationList.classList.remove("hidden");
+    }else{
+        locationList.classList.add("hidden")
+    }
+    
+}
+window.locChoose=locChoose;
